876c1b7cce2f4ee1766a4659e4eb6b47cd3b8e2d
DreamC <cmb02236@gmail.com>
2025-03-07 15:20:25 +0900

    OOPMain 클래스에 다형성 예제 추가 및 관련 코드 개선

--- OOPMain.java
+++ OOPMain.java
@@ -1,4 +1,5 @@
 import siat.study.oop.domain.constructor.ManagerVO;
+import siat.study.oop.domain.constructor.PersonVO;
 import siat.study.oop.domain.constructor.StudentVO;
 import siat.study.oop.domain.constructor.TeacherVO;
 
@@ -7,7 +8,7 @@ public class OOPMain {
      OOP(Object Oriented Programming)
      -은닉화
      -상속
-     -다형성
+     -다형성(타입, 매개변수. 메서드)
      -추상화
      */
     public static void main(String[] args) {
@@ -20,6 +21,34 @@ public class OOPMain {
         System.out.println(tea.teacherInfo());
         System.out.println(mg.perInfo());
         System.out.println(mg.managerInfo());
-        
+        /////////////////
+        PersonVO stu01=new StudentVO("Dream", "Incheon", "2018");
+        System.out.println(stu01.perInfo());
+        System.out.println(((StudentVO)stu01).stuInfo());
+        PersonVO tea01=new TeacherVO("JSLIM", "광주", "자바");
+        System.out.println(tea01.perInfo());
+        System.out.println(((TeacherVO)tea01).teacherInfo());
+        PersonVO mg01=new ManagerVO("이진규", "서울", "Siat");
+        System.out.println(mg01.perInfo());
+        System.out.println(((ManagerVO)mg01).managerInfo());
+        ///// 다형성을 사용하는 이유?
+        System.out.println("-----------다형성");
+        PersonVO [] perAry = new PersonVO[3];
+        perAry[0]=stu;
+        perAry[1]=tea;
+        perAry[2]=mg;
+        for(int i=0;i<perAry.length;i++){
+            PersonVO per =perAry[i];
+            // instanceof 연산자:객체타입을 비교해주는 연산자
+            if(per instanceof StudentVO){
+                System.out.println(((StudentVO)per).stuInfo());
+            }
+            if(per instanceof TeacherVO){
+                System.out.println(((TeacherVO)per).teacherInfo());
+            }
+            if(per instanceof ManagerVO){
+                System.out.println(((ManagerVO)per).managerInfo());
+            }
+        }
     }
 }
